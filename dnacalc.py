2c8ae2d04dc159876f786aa17f6cc11c1628b630
matsudad <matsudad@gmail.com>
2014-03-11 14:34:21 -0700

    3rd commit

--- dnacalc.py
+++ dnacalc.py
@@ -1,7 +1,25 @@
 #! /usr/bin/env python
 
-DNASeq = 'ATGAAC'
+# This program takes a DNA sequence and shows its length and the nucleotide composition
+
+DNASeq = raw_input("Enter a DNA sequence: ")
+DNASeq = DNASeq.upper()
+DNASeq = DNASeq.replace(" ","")
 print 'Sequence:', DNASeq
 
-SeqLength = len(DNASeq)
-print 'Sequence Length:', SeqLength
+SeqLength = float(len(DNASeq))
+print 'Sequence Length:', int(SeqLength)
+
+NumberA = DNASeq.count('A')
+NumberT = DNASeq.count('T')
+NumberG = DNASeq.count('G')
+NumberC = DNASeq.count('C')
+
+# Old way to output the Numbers, now removed
+# print 'A:", NumberA/SeqLength
+
+# Calculate percentage and out put to 1 decimal
+print 'There are %d A base(s) out of %d (%.1f%%)' % (NumberA, SeqLength, 100*NumberA/SeqLength)
+print 'There are %d T base(s) out of %d (%.1f%%)' % (NumberT, SeqLength, 100*NumberT/SeqLength)
+print 'There are %d G base(s) out of %d (%.1f%%)' % (NumberG, SeqLength, 100*NumberG/SeqLength)
+print 'There are %d C base(s) out of %d (%.1f%%)' % (NumberC, SeqLength, 100*NumberC/SeqLength)
