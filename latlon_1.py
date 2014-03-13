06c7335a4b9454c3235acf8b6cef6a2a9ed01187
matsudad <matsudad@gmail.com>
2014-03-13 16:39:09 -0700

    3rd commit

--- latlon_1.py
+++ latlon_1.py
@@ -13,10 +13,18 @@ LineNumber = 0
 # Loop through each line in the file 
 for Line in InFile:
 	if LineNumber > 0:
+	
 		# Remove the line-ending characters
-		Line = Line.strip('\n')
+		Line = Line.strip('\n').strip('\r')
+		
+		# Separate the line number in a list of its tab-delimited components
+		# This method takes a string and splits it according to a delimitter, thereby 
+		# producing a list of strings
+		ElementList = Line.split('\t')
+		
 		# Print the line
-		print LineNumber, ':', Line
+		print LineNumber, ':', ElementList
+		print "Depth: %s\tLat: %s\tLon: %s" % (ElementList[4], ElementList[2], ElementList[3])
 	
 	# Index the counter used to keep track of line numbers
 	LineNumber = LineNumber +1
