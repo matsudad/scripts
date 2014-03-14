bf7225e6a2d76e4e19e2bb04da020850af66dc84
matsudad <matsudad@gmail.com>
2014-03-14 10:55:28 -0700

    3rd commit

--- latlon_1.py
+++ latlon_1.py
@@ -10,6 +10,12 @@ InFile = open(InFileName, 'r')
 # Initialize the counter used to keep track of line numbers
 LineNumber = 0
 
+# Open the output file for writing
+#Do this *before* the loop, not inside it
+OutFileName = InFileName + ".kml"
+
+OutFile = open(OutFileName, 'w') # You can append instead with 'a'
+
 # Loop through each line in the file 
 for Line in InFile:
 	if LineNumber > 0:
@@ -22,12 +28,20 @@ for Line in InFile:
 		# producing a list of strings
 		ElementList = Line.split('\t')
 		
-		# Print the line
-		print LineNumber, ':', ElementList
-		print "Depth: %s\tLat: %s\tLon: %s" % (ElementList[4], ElementList[2], ElementList[3])
-	
+		# use the % operator to generate a string
+		# We can use this for output both to the screen and to a file
+		OutputString = "Depth: %s\tLat: %s\tLon: %s" % (ElementList[4], ElementList[2], ElementList[3])
+		
+		#can still print to the screen then write to a file
+		print OutputString
+		
+		#Unlike print statement, .write method need a linefeed
+		OutFile.write(OutputString + "\n")
+		
 	# Index the counter used to keep track of line numbers
 	LineNumber = LineNumber +1
 	
 # After the loop is completed, close the file
 InFile.close()
+OutFile.close()
+
