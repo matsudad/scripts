158001fccb93e6dcf00ad9b1fee91c12028a6e36
matsudad <matsudad@gmail.com>
2014-03-14 11:40:57 -0700

    fourth commit

--- latlon_1.py
+++ latlon_1.py
@@ -2,19 +2,63 @@
 
 # Set the inout file name
 # (The program must be run from within the directory that contains this data file)
+
+# This program reads in a file containing several columns of data 
+# and returns a file with decimal converted value and selected data fields.
+# The process is: Read in each line of the example file, split it into 
+# separate components, and write certain output to a separate file
+
+# Load regular expression module, used by decimalat()
+import re
+
+# Functions must be defined before they used
+def decimalat(DegString):
+	# This function requires that the re module is loaded
+	# Take a string in the format "34 56.78 N" and return decimal degree
+	SearchStr = '(\d+) ([\d\.]+) (\w)'
+	Result = re.search(SearchStr, DegString)
+	
+	# Get the captured character groups, as defined by the parenthases
+	# in the recular expression, convert the numbers to floats, and
+	# assign them to variables with meaningful names
+	Degrees = float(Result.group(1))
+	Minutes = float(Result.group(2))
+	Compass = Result.group(3).upper() #make sure it is capital too
+	
+	# Calculate the decimal degrees
+	DecimalDegree = Degrees + Minutes/60
+	
+	# If the compass direction indicates the coordinate is South or West,
+	# make sign of the coordinate negative
+	if Compass == 'S' or 'N':
+		DecimalDegree = -DecimalDegree
+	return DecimalDegree
+
+# End of function decimalat() definition
+ 	
+# Set input file name
 InFileName = "Marrus_claudanielis.txt"
+
+# Derive the output file name from the input file name
+OutFileName = 'dec_' + InFileName
+
+# Give the option to write to a file or just print to screen
+WriteOutFile = True
 
 # Open the input file for reading
 InFile = open(InFileName, 'r')
 
+HeaderLine = 'drive\tdepth\tlatitute\tlontitute\tdate\tcomment'
+print HeaderLine
+
+# Open the output file, if desired. Do this outside the loop
+if WriteOutFile:
+	# Open the outputfile
+	OutFile = open(OutFileName, 'w') # You can append instead with 'a'
+	OutFile.write(HeaderLine + '\n')
+	
 # Initialize the counter used to keep track of line numbers
 LineNumber = 0
-
-# Open the output file for writing
-#Do this *before* the loop, not inside it
-OutFileName = InFileName + ".kml"
-
-OutFile = open(OutFileName, 'w') # You can append instead with 'a'
 
 # Loop through each line in the file 
 for Line in InFile:
@@ -28,20 +72,32 @@ for Line in InFile:
 		# producing a list of strings
 		ElementList = Line.split('\t')
 		
-		# use the % operator to generate a string
-		# We can use this for output both to the screen and to a file
-		OutputString = "Depth: %s\tLat: %s\tLon: %s" % (ElementList[4], ElementList[2], ElementList[3])
+		# Returns a list in this format:
+		Dive 	= ElementList[0]
+		Date 	= ElementList[1]
+		Depth 	= ElementList[4]
+		Comment = ElementList[5]
+		
+		LatDegrees = decimalat(ElementList[2])
+		LonDegrees = decimalat(ElementList[3])
+		
+		# Create string to 5 decimal places, padded to 10 total characters
+		# using line continuation character \
+		OutString = "%s\t%4s\t%10.5f\t%10.5f\t%9s\t%s" % (Dive, Depth, LatDegrees, LonDegrees, Date, Comment)
 		
 		#can still print to the screen then write to a file
-		print OutputString
-		
-		#Unlike print statement, .write method need a linefeed
-		OutFile.write(OutputString + "\n")
+		print OutString
+		if WriteOutFile:
+			OutFile.write(OutString + '\n') #remember the line feed!
+	
 		
 	# Index the counter used to keep track of line numbers
 	LineNumber = LineNumber +1
 	
 # After the loop is completed, close the file
 InFile.close()
-OutFile.close()
+
+if WriteOutFile:
+	OutFile.close()
+	
 
